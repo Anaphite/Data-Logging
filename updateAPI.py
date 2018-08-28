8464ab156321610048472c19d5cc30717eef15ba
gw97 <41743460+gw97@users.noreply.github.com>
2018-08-28 14:51:07 +0100

    Create updateAPI.py
    updateAPI temperature version

---
+++ updateAPI.py
@@ -0,0 +1,24 @@
+#script to install latest version from API repository from GitHub
+#script to install latest version from API repository from GitHub
+
+import os
+import time
+os.system
+#deletes old programs
+
+os.system("rm TGpub.py")
+os.system("rm VOCGpub.py")
+os.system("rm updateAPI.py")
+#os.system("rm updateAPI.py")
+#os.system("rm README.md")
+#clones repository
+os.system("git clone https://github.com/gw97/API.git")
+time.sleep(20)
+#moves files out of API file to where update.py stored
+os.system("mv /home/pi/Temperature/API/TGpub.py /home/pi/Temperature/TGpub.py")
+os.system("mv /home/pi/Temperature/API/VOCGpub.py /home/pi/Temperature/VOCGpub.py")
+os.system("mv /home/pi/Temperature/API/updateAPI.py /home/pi/Temperature/updateAPI.py")
+
+
+#removes the API directory now relavent contents have been removed
+os.system("rm -r -f /home/pi/Temperature/API")
