4b1ff93d70a2ec95216eb6a36ec34352a26c1ae2
gw97 <41743460+gw97@users.noreply.github.com>
2018-08-30 09:59:28 +0100

    Add files via upload
    simple updating system

---
+++ update.py
@@ -0,0 +1,10 @@
+import os
+import time
+
+#removes old repository
+os.system("rm -r -f Data-Logging")
+
+#gets new repository
+
+os.system("git clone https://github.com/Anaphite/Data-Logging.git")
+ 
