7f3617c8219c0bd407c061b0e03063c55500d490
gw97 <41743460+gw97@users.noreply.github.com>
2018-08-30 10:17:24 +0100

    Update update.py
    test

--- update.py
+++ update.py
@@ -2,11 +2,11 @@ import os
 import time
 
 #removes old repository
-os.system("rm -r -f /home/pi/Temperature/Data-Logging")
+os.system("rm -r -f Data-Logging")
 
 #gets new repository
 
 os.system("git clone https://github.com/Anaphite/Data-Logging.git")
 
-os.system("mv update.py")
+os.system("mv update.py ../update.py")
  
