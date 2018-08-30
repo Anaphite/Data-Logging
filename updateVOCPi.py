efa81634e0905209b70db612e2dac360c0d621a2
gw97 <41743460+gw97@users.noreply.github.com>
2018-08-30 10:52:06 +0100

    Update and rename updateVOCpi.py to updateVOCPi.py
    not ready - paths need changing to the correct ones in line 13

---
+++ updateVOCPi.py
@@ -0,0 +1,13 @@
+import os
+import time
+#test6
+
+os.system("rm updateVOCPi.py")
+#removes old repository
+os.system("rm -r -f Data-Logging")
+
+#gets new repository
+
+os.system("git clone https://github.com/Anaphite/Data-Logging.git")
+#paths need changeing to the approriate ones for the pi zero
+os.system("mv /home/pi/Temperature/Data-Logging/updateVOCPi.py /home/pi/Temperature/updateVOCPi.py")
