6fd4c25996b19922151c6cb3c92eb57674f29657
gw97 <41743460+gw97@users.noreply.github.com>
2018-08-31 09:57:08 +0100

    Update updateVOCPi.py
    correct file paths added for the VOC paths

--- updateVOCPi.py
+++ updateVOCPi.py
@@ -10,4 +10,4 @@ os.system("rm -r -f Data-Logging")
 
 os.system("git clone https://github.com/Anaphite/Data-Logging.git")
 #paths need changeing to the approriate ones for the pi zero
-os.system("mv /home/pi/Temperature/Data-Logging/updateVOCPi.py /home/pi/Temperature/updateVOCPi.py")
+os.system("mv /home/pi/Vlog/Data-Logging/updateVOCPi.py /home/pi/Vlog/updateVOCPi.py")
